ef0993613a4d01508db91a6d6a697abd7f0a2e17
vanya890 <vanya.kuzyk1@gmail.com>
2026-05-10 19:04:09 +0300

    Phase 10: Update src/App.tsx with SeedData and proper structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { Layout } from './components/Layout'
+import { SeedData } from './components/SeedData'
 import { GalleryPage } from './pages/GalleryPage'
 import { ChatsPage } from './pages/ChatsPage'
 import { CharactersPage } from './pages/CharactersPage'
 import { ChatPage } from './pages/ChatPage'
 import { SettingsPage } from './pages/SettingsPage'
 import { useAppStore } from './stores/appStore'
+import { useEffect } from 'react'
 
-function App() {
-  const { initialized } = useAppStore()
+function AppContent() {
+  const { initialized, init } = useAppStore()
+
+  useEffect(() => {
+    init()
+  }, [init])
 
   if (!initialized) {
     return (
@@ -20,17 +27,20 @@ function App() {
   }
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/gallery" replace />} />
-        <Route path="gallery" element={<GalleryPage />} />
-        <Route path="chats" element={<ChatsPage />} />
-        <Route path="characters" element={<CharactersPage />} />
-        <Route path="chat/:chatId" element={<ChatPage />} />
-        <Route path="settings" element={<SettingsPage />} />
-      </Route>
-    </Routes>
+    <>
+      <SeedData />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/gallery" replace />} />
+          <Route path="gallery" element={<GalleryPage />} />
+          <Route path="chats" element={<ChatsPage />} />
+          <Route path="characters" element={<CharactersPage />} />
+          <Route path="chat/:chatId" element={<ChatPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
-export default App
+export default AppContent
